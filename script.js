ed43b0d8cac5b30782788a3989f1e12cb30d1a21
PHP Developer <vikram.singh@vortilla.com>
2023-05-12 07:28:48 +0530

    change

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 
 
 const updateTime = () =>{
-    var datetime = new Date();
-    var hours = datetime.getHours();
-    var minutes = datetime.getMinutes();
-    var seconds = datetime.getSeconds();
+    var datetim = new Date();
+    var hour = datetim.getHours();
+    var minute = datetim.getMinutes();
+    var second = datetim.getSeconds();
     var am_and_pm = document.getElementById("am-and-pm");
 
-    if(hours >= 12){
+    if(hour >= 12){
         am_and_pm.innerHTML= "PM";
     }else{
         am_and_pm="AM";
     }
 
-    if(hours>12){
-        hours = hours - 12;
+    if(hour>12){
+        hour = hour - 12;
     }
 
     
-    document.getElementById("hours").innerHTML= hours;
-    document.getElementById("minutes").innerHTML= minutes;
-    document.getElementById("seconds").innerHTML= seconds;
-    // document.getElementById("am_and_pm")= am_and_pm;
+    document.getElementById("hours").innerHTML= hour;
+    document.getElementById("minutes").innerHTML= minute;
+    document.getElementById("seconds").innerHTML= second;
 }
 
-setInterval(updateTime,1000);
+// setInterval(updateTime,1000);
+// number is liye hai ki refres krne par kitna time lena hai 
+setInterval(updateTime);
 
